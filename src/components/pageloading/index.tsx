590decaea37e6b67fc5507392efa7ba685617ea3
Narekmaralchyan <narekmaralchyan@gmail.com>
2022-12-22 18:50:12 +0400

    new page loading interface

--- src/components/pageloading/index.tsx
+++ src/components/pageloading/index.tsx
@@ -3,35 +3,10 @@ import './style.css';
 const Pageloading = () => {
     return(
         <div className="background">
-            <div className="container">
-                <h1>Loading</h1>
-                <h3>Please wait</h3>
-                <div className="wrapper">
-                    <div className="loader">
-                        <div className="spinner"></div>
-                    </div>
-                    <div className="loader">
-                        <div className="spinner"></div>
-                    </div>
-                    <div className="loader">
-                        <div className="spinner"></div>
-                    </div>
-                    <div className="loader">
-                        <div className="spinner"></div>
-                    </div>
-                    <div className="loader">
-                        <div className="spinner"></div>
-                    </div>
-                    <div className="loader">
-                        <div className="spinner"></div>
-                    </div>
-                    <div className="loader">
-                        <div className="spinner"></div>
-                    </div>
-                    <div className="loader">
-                        <div className="spinner"></div>
-                    </div>
-                </div>
+            <div className="spinner">
+                <div className="circle one"></div>
+                <div className="circle two"></div>
+                <div className="circle three"></div>
             </div>
         </div>
     );
