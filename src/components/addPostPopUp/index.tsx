1b80a7316e07d12db607f652dd63e47abc2dc4b3
Narekmaralchyan <narekmaralchyan@gmail.com>
2022-12-24 00:24:36 +0400

    fix add post bugs

--- src/components/addPostPopUp/index.tsx
+++ src/components/addPostPopUp/index.tsx
@@ -13,28 +13,32 @@ interface IProps {
 const AddPostPopUp:FC<IProps> = ({handleShowAddPostPopUp})=>{
     const [showAlert,setShowAlert] = useState(false);
     const [loading,setLoading] = useState(false);
-    const [file,setFile] = useState(null);
+    const [file,setFile] = useState<null | File>(null );
     const [description,setDescription] = useState('');
     const {data} = useAppSelector(state=>state.currentUser);
     const currentUserAPI = new UserAPI(data || '');
 
     function handleFileInputChange(e:ChangeEvent<HTMLInputElement>){
-       setFile(e.target.files[0]);
+        if(e.target.files){
+            setFile(e.target.files[0]);
+        }
     }
     function handleChangeDescriptionInput(e:ChangeEvent<HTMLInputElement>){
         setDescription(e.target.value);
     }
     function addPost(){
-        setLoading(true);
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = function (){
-            currentUserAPI.addPost(reader.result as string,description)
-                .then(()=>{
-                    setLoading(false);
-                    setShowAlert(true);
-                });
-        };
+        if(file){
+            setLoading(true);
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = function (){
+                currentUserAPI.addPost(reader.result as string,description)
+                    .then(()=>{
+                        setLoading(false);
+                        setShowAlert(true);
+                    });
+            };
+        }
     }
 
     return (
@@ -54,7 +58,7 @@ const AddPostPopUp:FC<IProps> = ({handleShowAddPostPopUp})=>{
             <div className="popUp">
                 <img alt="added_image" src={file && URL.createObjectURL(file) || 'https://cdn.pixabay.com/photo/2017/04/20/07/07/add-2244771_960_720.png'}/>
                 <div className="manage">
-                    <Input   accept="image/*" sx={{width:'105px'}} onChange={handleFileInputChange} type="file"/>
+                    <Input inputProps={{ accept: 'image/*' }}   sx={{width:'105px'}} onChange={handleFileInputChange} type="file"/>
                     <Input value={description} placeholder="add description" type="text" onChange={handleChangeDescriptionInput} />
                    <div className="buttons">
                        <Button onClick={addPost} disabled={!file} variant="contained">Add</Button>
