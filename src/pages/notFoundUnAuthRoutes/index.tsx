a810aba8fc6b92e20be71ebacf42347191037414
Anush Movsesyan <AnushMv@users.noreply.github.com>
2022-12-24 01:31:50 +0400

    create notFoundUnAuthpage

---
+++ src/pages/notFoundUnAuthRoutes/index.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+
+const NotFoundUnAuthRouts: FC = () => {
+
+    return <div>
+        <h1>Not found this page</h1>
+        <button>Go to log in page</button>
+    </div>;
+};
+
+
+
+export default NotFoundUnAuthRouts;
