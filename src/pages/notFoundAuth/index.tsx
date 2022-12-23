b019d71d10d878e9333e2ac1ba61dadc316846e4
Anush Movsesyan <AnushMv@users.noreply.github.com>
2022-12-24 00:20:23 +0400

    create notFoundAuth page

---
+++ src/pages/notFoundAuth/index.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+
+const NotFoundAuthRouts: FC = () => {
+
+    return <div>
+        <h1>Not found this page</h1>
+        <button>Return to feed</button>
+    </div>;
+};
+
+
+
+export default NotFoundAuthRouts;
